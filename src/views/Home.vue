2f1a86d783dab1b083f61fc6c1ebbccb5309704a
Lionel Mann <lionelmann@gmail.com>
2018-01-19 15:06:36 -0500

    start of grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,38 @@
 <template>
-<div>
-    
-Welcome
- 
+<div class="container">
+  <div class="item">1</div>
+  <div class="item">2</div>
+  <div class="item">3</div>
+  <div class="item">4</div>
+  <div class="item">5</div>
+  <div class="item">6</div>
+  <div class="item item7">7</div>
+  <div class="item">8</div>
+  <div class="item">9</div>
+  <div class="item">10</div>
+  <div class="item">11</div>
+  <div class="item">12</div>
+  <div class="item">13</div>
 </div>
 </template>
 
-<script>
-	
-	export default {
-		
-	};
-</script>
+<style lang="scss" scoped>
 
-<style lang="sass" scoped>
+.item {
+  background-color: #ccc;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 200px auto;
+  grid-template-rows: 100px 500px; // explicit rows
+  grid-gap: 20px;
+  grid-auto-rows: 100px; //for any implicit rows that the browser creates
+}
+
+.item7 {
+  background: blue;
+  grid-column: span 2; // span columns
+  grid-row: span 2;
+}
 </style>
