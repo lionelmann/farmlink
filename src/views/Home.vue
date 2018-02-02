af13da502a4cde02c7c1e37cf509dd53a441d3a7
Lionel Mann <lionelmann@gmail.com>
2018-02-02 14:49:38 -0500

    home build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container">
-    <div class="banner-container" style="background-image: url('/assets/farmlink_background.png');">
+    <div class="banner-container">
         <div class="banner-text" style="width: 75%;">
             <h2 style="color: #517058; font-weight: 500;">Farmlink.net</h2>
             <h3>Connecting <span style="min-width: 110px; display:inline-block;"> <ICountUp
@@ -130,6 +130,113 @@
         </div>
     </div>
 
+   <div class="grid-wrapper">
+    <div class="ad-container" style="border-top: 1px solid #ccc; border-bottom: 1px solid #ccc;
+    padding: 40px; margin: 20px 0 60px 0;">
+        <h5>Register for $5/month and connect with farmers and seekers.</h5>
+    </div>
+    </div>
+
+    <div class="grid-wrapper">
+        <h5 style="font-weight: 500;">Featured Regions <a href="#" style="text-decoration: none;"><span style="font-size: 16px; font-weight: 200;" > See All Regions > </span></a></h5>
+        <div class="grid-container">
+
+            <a href="#">
+                <div class="grid-items">
+                    <img src="https://source.unsplash.com/1600x900/?farm">
+                    <div class="grid-card-content">
+                        <h6>Wellington County</h6>
+                        <ul>
+                            <li>4 Farms</li>
+                            <li>Ontario</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="grid-items">
+                    <img src="https://source.unsplash.com/1600x900/?crop">
+                    <div class="grid-card-content">
+                        <h6>Minto</h6>
+                        <ul>
+                            <li>10 Farms</li>
+                            <li>Manitoba</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="grid-items">
+                    <img src="https://source.unsplash.com/1600x900/?rural">
+                    <div class="grid-card-content">
+                        <h6>Prince Edward County</h6>
+                        <ul>
+                            <li>21 Farms</li>
+                            <li>Ontario</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="grid-items">
+                    <img src="https://source.unsplash.com/1600x900/?grain">
+                    <div class="grid-card-content">
+                        <h6>Elgen</h6>
+                        <ul>
+                            <li>1 Farm</li>
+                            <li>Saskatchwan</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="grid-wrapper">
+        <h5 style="font-weight: 500;">Recent Resources<a href="#" style="text-decoration: none;"><span style="font-size: 16px; font-weight: 200;" > See All Resources > </span></a></h5>
+        <div class="grid-container">
+
+            <a href="#">
+                <div class="grid-items">
+                    <div class="grid-card-content">
+                        <h6>Farm Succession Tools And Methods To Promote A Successful Farm Succession</h6>
+                        <p>In this resource the European Farm Succession Network (a network of organizations from France, Belgium, Austria and the Netherlands) gives new answers to changes in farm continuity and succession. ...</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="grid-items">
+                    <div class="grid-card-content">
+                        <h6>Successful Farming on a Land Cooperative</h6>
+                       <p>This case study, written for Land For Good details one person’s experience in joining an existing “land cooperative” in Vermont. She describes her experience with the cooperative and also provides supporting information </p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="grid-items">
+                    <div class="grid-card-content">
+                        <h6>Financing Agricultural Co-operatives</h6>
+                        <p>This overview details what agricultural co-operatives are, acquiring capital, accumulating equity, base capital plans and new generation co-operatives.</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="grid-items">
+                    <div class="grid-card-content">
+                        <h6>Agricultural Co-operatives: A Start-up Guide</h6>
+                        <p>This guide covers topics such as the basics of a co-operative, how to start a co-op, guidelines for success and resources.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
 
 </div>
 </template>
@@ -173,6 +280,7 @@
 }
 
 .banner-container {
+    background-image: url('/assets/farmlink_background.png');
     color: black;
     display: grid;
     align-content: center;
