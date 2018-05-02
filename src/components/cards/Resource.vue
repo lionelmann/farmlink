27b3216d8ecdeec5534d751d2d42ac67064023d2
Lionel Mann <lionelmann@gmail.com>
2018-05-02 12:06:55 -0400

    slide out

--- src/components/cards/Resource.vue
+++ src/components/cards/Resource.vue
@@ -1,19 +1,20 @@
 <template>
     <div>
-        <div v-on:click="isSlideout = !isSlideout">
+        <div @click="isSlideout = !isSlideout">
         <div class="card-content">
             <h5 v-html="resourceCard.title.rendered"></h5>
             <div class="light" v-html="$options.filters.readMore(resourceCard.content.rendered, 200, '...')"></div>               
         </div>
         </div>
-       
+       <div class="position: relative">
         <div v-if="!isSlideout" class="slideout">
-            <button style="position: absolute; top: 1em; left: 1em; z-index: 5001; border: none; cursor: pointer; outline: none;" v-on:click="isSlideout = !isSlideout"><i class="far fa-times-circle fa-3x"></i></button>
-            <div style="margin-top: 3em">
+            <button style="position: fixed; top: 140px; right: 38%; z-index: 5001; border: none; cursor: pointer; outline: none;" v-on:click="isSlideout = !isSlideout"><i class="far fa-times-circle fa-3x"></i></button>
+            <div style="margin: 4em 0 0 0; height: 95%; padding: 0 2em;">
                 <h5 v-html="resourceCard.title.rendered"></h5>
                 <div v-html="resourceCard.content.rendered"></div>
                 
             </div>
+        </div>
         </div>
     </div>
 </template>
@@ -35,17 +36,17 @@ export default {
 html, body { height: 100%; }
 
 .slideout {
-    position: absolute;
-    right: 0;
-    bottom:0;
-    height: 100vh;
-    overflow: scroll;
+    position: fixed;
+    top: 125px;
+    right: 0px;
+    bottom:0px;
+    height: 100%;
+    overflow-y: scroll;
     background: white;
     z-index: 3000;
     transition: width 2s;
-    width: 400px;
-    padding: 2em;
-     box-shadow: 0 1px 3px rgba(0,0,0,0.1), 0 1px 2px rgba(0,0,0,0.20);
+    width: 40%;
+    box-shadow: 0 1px 0px rgba(0,0,0,0.1), 0 1px 2px rgba(0,0,0,0.20);
 
 }
 
