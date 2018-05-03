3daa6bdb191b7b7d5ebeddb3fcb335265f723a78
Lionel Mann <lionelmann@gmail.com>
2018-05-03 15:38:34 -0400

    single seeker styles

--- src/views/SingleSeeker.vue
+++ src/views/SingleSeeker.vue
@@ -1,70 +1,69 @@
 <template>
     <div style="background-color: rgba(69,105,81, 1);">
         <div v-if="seeker != null">
-            <div class="top">
+            <div class="outer-container">
+            
                 <div class="hero">
                     <h3 v-html="seeker.meta.first_name[0]"></h3>
+                     <time>Last Modified: {{ moment(seeker.meta.user_mod_date[0]) }}</time>
                 </div>
             </div>
 
-            <aside>
-                        <time>Last Modified: {{ moment(seeker.meta.user_mod_date[0]) }}</time>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_provinces')">
-                            <li class="title">Looking for a farm in:</li>
-                            <li v-for="item in seeker.meta._seeker_provinces" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_opportunity')">
-                            <li class="title">Following opportunities:</li>
-                            <li v-for="item in seeker.meta._seeker_opportunity" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_years')">
-                            <li class="title">Number of years farming:</li>
-                            <li v-for="item in seeker.meta._seeker_years" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_experience')">
-                            <li class="title">Farming experience:</li>
-                            <li v-for="item in seeker.meta._seeker_experience" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_products')">
-                            <li class="title">Agricultural products:</li>
-                            <li v-for="item in seeker.meta._seeker_products" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_marketing')">
-                            <li class="title">Planned marketing strategies:</li>
-                            <li v-for="item in seeker.meta._seeker_marketing" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_land')">
-                            <li class="title">Land needed:</li>
-                            <li v-for="item in seeker.meta._seeker_land" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_facilities')">
-                            <li class="title">Facilities and Equipment:</li>
-                            <li v-for="item in seeker.meta._seeker_facilities" :key="item.id" v-html="item"></li>
-                        </ul>
-                        <ul v-if="seeker.meta.hasOwnProperty('_seeker_practices')">
-                            <li class="title">Planned farming practices:</li>
-                            <li v-for="item in seeker.meta._seeker_practices" :key="item.id" v-html="item"></li>
-                        </ul>
-                    </aside>
+             <div class="outer-container">
+                <h3>Stats</h3>   
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_provinces')">
+                    <li class="title">Looking for a farm in:</li>
+                    <li v-for="item in seeker.meta._seeker_provinces" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_opportunity')">
+                    <li class="title">Following opportunities:</li>
+                    <li v-for="item in seeker.meta._seeker_opportunity" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_years')">
+                    <li class="title">Number of years farming:</li>
+                    <li v-for="item in seeker.meta._seeker_years" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_experience')">
+                    <li class="title">Farming experience:</li>
+                    <li v-for="item in seeker.meta._seeker_experience" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_products')">
+                    <li class="title">Agricultural products:</li>
+                    <li v-for="item in seeker.meta._seeker_products" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_marketing')">
+                    <li class="title">Planned marketing strategies:</li>
+                    <li v-for="item in seeker.meta._seeker_marketing" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_land')">
+                    <li class="title">Land needed:</li>
+                    <li v-for="item in seeker.meta._seeker_land" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_facilities')">
+                    <li class="title">Facilities and Equipment:</li>
+                    <li v-for="item in seeker.meta._seeker_facilities" :key="item.id" v-html="item"></li>
+                </ul>
+                <ul class="stats-list" v-if="seeker.meta.hasOwnProperty('_seeker_practices')">
+                    <li class="title">Planned farming practices:</li>
+                    <li v-for="item in seeker.meta._seeker_practices" :key="item.id" v-html="item"></li>
+                </ul>
+            </div>
 
-            <div class="outer-container" style="max-width: 768px; margin: auto auto; padding: 10px;">
-                <h3 style="color: rgba(255,255,255, 1);">About</h3>
-                <div class="inner-container" style="background-color: rgba(55,83,65, 1); padding: 30px;">
-                    <h4 style="color: rgba(255,255,255, 1);">Overview</h4>
-                    <p v-html="seeker.description" style="padding: 20px; color: rgba(255,255,255, 0.7); white-space:pre-wrap"></p>
+            <div class="outer-container">
+                <h3>About</h3>
+                <div class="inner-container">
+                    <h4>Overview</h4>
+                    <p v-html="seeker.description"></p>
                     <div v-if="seeker.meta.hasOwnProperty('_seeker_rel_experience')">
-                        <h4 style="color: rgba(255,255,255, 1);">Experience</h4>
+                        <h4>Experience</h4>
                         <p v-html="seeker.meta._seeker_rel_experience[0]"></p>
                     </div>
                     <div v-if="seeker.meta.hasOwnProperty('_seeker_partners')">
-                        <h4 style="color: rgba(255,255,255, 1);">Partners</h4>
+                        <h4>Partners</h4>
                         <p v-html="seeker.meta._seeker_partners[0]"></p>
                     </div>
                 </div>
             </div>
-
-
-            
         </div>
     </div>
 </template>
@@ -95,11 +94,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.top {
-    display: grid;
-    border: 1px solid red;
-}
 
 .hero {
     display: grid;
