7e31bf4cd48634e21d8516ae6fd6c995edff9459
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-05-03 10:43:44 -0400

    Updates listings map

--- src/views/Listings.vue
+++ src/views/Listings.vue
@@ -123,9 +123,6 @@ export default {
         // Store 'this' in a variable, so you can referecne 'this' properly in
         // the following fucntions
         let app = this;
-
-        app.bounds     = new google.maps.LatLngBounds();
-
         const element   = document.getElementById('google-map');
         // const mapCentre = this.markerCoordinates[0]
         const options   = {
@@ -143,7 +140,8 @@ export default {
             styles: [{"featureType": 'poi.park', "elementType": 'geometry',"stylers": [{"color": '#137E23'},{ "visibility": "on" }]},{"featureType":"poi.business","stylers": [{ "visibility": "off" }]},{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
         }
 
-        app.map = new google.maps.Map(element, options);
+        app.map     = new google.maps.Map(element, options);
+        // app.bounds  = new google.maps.LatLngBounds();
         
         app.buildMarkers();
     },
